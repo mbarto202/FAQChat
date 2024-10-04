efb6950ba6fb377261f393b0a16e535ed44e683f
mbarto202 <michael.d.barto@gmail.com>
2024-10-04 15:34:36 -0400

    Navbar has a foundation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styles from './Navbar.module.css';
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <div>navbar</div>
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>FaqChat.co</div>
+      <ul className={styles.navLinks}>
+        <li className={styles.productsOption}><a href="#products">Products</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#pricing">Pricing</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
   );
-};
-
-export default Navbar;
+}
