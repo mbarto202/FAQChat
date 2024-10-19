364a33c9b3381670524146119ffb09ff5a8145e7
mbarto202 <michael.d.barto@gmail.com>
2024-10-19 00:18:30 -0400

    TryChatting created with some css

--- app/components/TryChatting/TryChatting.tsx
+++ app/components/TryChatting/TryChatting.tsx
@@ -1,9 +1,25 @@
+"use client";
+
 import React from 'react';
+import Image from 'next/image';
+import styles from './TryChatting.module.css'; 
 
 const TryChatting = () => {
   return (
-    <section id="try-chatting">
-     <div>try chatting</div>
+    <section className={styles.tryChattingContainer}>
+      {/* Left section (title, text, button) */}
+      <div className={styles.leftSection}>
+        <h2 className={styles.title}>Try Chatting with Our FAQ</h2>
+        <p className={styles.description}>
+          Experience our FAQChat system and see how it works
+        </p>
+        <button className={styles.chatButton}>Chat Here</button>
+      </div>
+
+      {/* Right section (PNG image) */}
+      <div className={styles.rightSection}>
+        png
+      </div>
     </section>
   );
 };
