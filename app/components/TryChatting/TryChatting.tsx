3eaec2c30f08be5425fa7b1634d201e71821bd72
mbarto202 <michael.d.barto@gmail.com>
2024-10-21 00:15:29 -0400

    installed prettier

--- app/components/TryChatting/TryChatting.tsx
+++ app/components/TryChatting/TryChatting.tsx
@@ -1,24 +1,28 @@
 "use client";
 
-import React from 'react';
-import Image from 'next/image';
-import styles from './TryChatting.module.css'; // Import the CSS file
+import React from "react";
+import Image from "next/image";
+import styles from "./TryChatting.module.css"; // Import the CSS file
 
 const TryChatting = () => {
   return (
     <section className={styles.tryChattingSection}>
-      <div className={styles.contentFrame}> {/* Frame encapsulating the title, text, and button */}
+      <div className={styles.contentFrame}>
+        {" "}
+        {/* Frame encapsulating the title, text, and button */}
         <div className={styles.titleTextFrame}>
           <div className="titleFrame">
             <h2 className={styles.title}>Try Chatting with Our FAQ</h2>
           </div>
-          <p className={styles.description}>Experience our FAQChat system and see how it works</p>
-          <button className={styles.chatButton}>Chat Here</button>
+          <p className={styles.description}>
+            Experience our FAQChat system and see how it works
+          </p>
+          <div className={styles.chatButtonFrame}>
+            <button className={styles.chatButton}>Chat Here</button>
+          </div>
         </div>
       </div>
-      <div className={styles.imageContainer}>
-        png
-      </div>
+      <div className={styles.imageContainer}>png</div>
     </section>
   );
 };
