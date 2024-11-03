afc0e474b986b643da201ef09cd049a229f75990
mbarto202 <michael.d.barto@gmail.com>
2024-11-03 18:49:02 -0500

    created new section

--- app/components/CreateYourOwn/CreateYourOwn.tsx
+++ app/components/CreateYourOwn/CreateYourOwn.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import styles from "./CreateYourOwn.module.css";
 
 const CreateYourOwn = () => {
   return (
-    <section id="create-your-own">
-      <div>create your own</div>
+    <section className={styles.container}>
+      <div className={styles.titleTextFrame}>
+        <h2>Create your own FAQChat now.</h2>
+        <p>
+          Upload text of any length below, and we will convert it into an
+          organized FAQ that your audience can chat with.
+        </p>
+      </div>
     </section>
   );
 };
