c5a8ccc36b4be56d9e2f8c1b8da518fd134ab8b9
mbarto202 <michael.d.barto@gmail.com>
2024-11-21 23:24:26 -0500

    starting new section

--- app/components/UsersLoveIt/UsersLoveIt.tsx
+++ app/components/UsersLoveIt/UsersLoveIt.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import styles from "./UsersLoveIt.module.css";
 
 const UsersLoveIt = () => {
   return (
     <section id="users-love-it">
-      <div></div>
+      <div className={styles.container}>
+        <div className={styles.titleContainer}>
+          <h2>Users love it because:</h2>
+        </div>
+        <div className={styles.contentContainer}>
+          <p>Content div</p>
+        </div>
+      </div>
     </section>
   );
 };
