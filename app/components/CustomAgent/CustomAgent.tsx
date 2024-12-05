4c4bd734f8043ef518daa7f304bb44e98e9437e5
mbarto202 <michael.d.barto@gmail.com>
2024-12-05 00:00:07 -0500

    title content added

--- app/components/CustomAgent/CustomAgent.tsx
+++ app/components/CustomAgent/CustomAgent.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import styles from "./CustomAgent.module.css";
 
 const CustomAgent = () => {
   return (
     <section id="custom-agent">
-      <div></div>
+      <div className={styles.container}>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.titleText}>Want a custom agent?</h2>
+          <p className={styles.smallerText}>
+            Leave your email here and we’ll contact you.
+          </p>
+        </div>
+        <div className={styles.emailForm}>Email form</div>
+      </div>
     </section>
   );
 };
