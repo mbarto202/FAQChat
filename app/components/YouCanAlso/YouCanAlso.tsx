7b6555e0e60061c98d7418e44b5358042fa29fb5
mbarto202 <michael.d.barto@gmail.com>
2024-12-10 23:19:06 -0500

    all content added to tsx

--- app/components/YouCanAlso/YouCanAlso.tsx
+++ app/components/YouCanAlso/YouCanAlso.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import styles from "./YouCanAlso.module.css";
+import Button from "../Button/Button";
 
 const YouCanAlso = () => {
   return (
     <section id="you-can-also">
-      <div></div>
+      <div className={styles.container}>
+        <img
+          src="/path-to-your-image.jpg"
+          alt="Description of image"
+          className={styles.image}
+        />
+        <div className={styles.textContainer}>
+          <h2 className={styles.title}>
+            You can also try chatting with ours. It’s fun!
+          </h2>
+          <Button text="Generate AI Agent" />
+        </div>
+      </div>
     </section>
   );
 };
