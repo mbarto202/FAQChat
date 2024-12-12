50b6a1db43ed15eaff3c603265b6889f5cb301a1
mbarto202 <michael.d.barto@gmail.com>
2024-12-11 23:20:07 -0500

    section finished for bigger screens

--- app/components/YouCanAlso/YouCanAlso.tsx
+++ app/components/YouCanAlso/YouCanAlso.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import styles from "./YouCanAlso.module.css";
 import Button from "../Button/Button";
+import img from "../../assets/images/image2.png";
+import Image from "next/image";
 
 const YouCanAlso = () => {
   return (
     <section id="you-can-also">
       <div className={styles.container}>
-        <img
-          src="/path-to-your-image.jpg"
-          alt="Description of image"
-          className={styles.image}
-        />
+        <div className={styles.ellipse} />
+        <Image src={img} alt="Description of image" className={styles.image} />
         <div className={styles.textContainer}>
           <h2 className={styles.title}>
             You can also try chatting with ours. It’s fun!
           </h2>
-          <Button text="Generate AI Agent" />
+          <Button text="Start Chatting" />
         </div>
       </div>
     </section>
