aa5e111e28bffe18cfcf3da95765fd90c7007741
mbarto202 <michael.d.barto@gmail.com>
2024-12-11 23:31:40 -0500

    working on responsiveness for smaller screens

--- app/components/YouCanAlso/YouCanAlso.tsx
+++ app/components/YouCanAlso/YouCanAlso.tsx
@@ -6,16 +6,14 @@ import Image from "next/image";
 
 const YouCanAlso = () => {
   return (
-    <section id="you-can-also">
-      <div className={styles.container}>
-        <div className={styles.ellipse} />
-        <Image src={img} alt="Description of image" className={styles.image} />
-        <div className={styles.textContainer}>
-          <h2 className={styles.title}>
-            You can also try chatting with ours. It’s fun!
-          </h2>
-          <Button text="Start Chatting" />
-        </div>
+    <section id="you-can-also" className={styles.container}>
+      <div className={styles.ellipse} />
+      <Image src={img} alt="Chatter" className={styles.image} />
+      <div className={styles.textContainer}>
+        <h2 className={styles.title}>
+          You can also try chatting with ours. It’s fun!
+        </h2>
+        <Button text="Start Chatting" />
       </div>
     </section>
   );
