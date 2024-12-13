fe4297af9b30031fb5e844eedf0d681d7a78f9b1
mbarto202 <michael.d.barto@gmail.com>
2024-12-13 17:12:49 -0500

    optimized code

--- app/components/YouCanAlso/YouCanAlso.tsx
+++ app/components/YouCanAlso/YouCanAlso.tsx
@@ -1,22 +1,20 @@
 import React from "react";
 import styles from "./YouCanAlso.module.css";
 import Button from "../Button/Button";
-import img from "../../assets/images/image2.png";
 import Image from "next/image";
+import img from "../../assets/images/image2.png";
 
-const YouCanAlso = () => {
-  return (
-    <section id="you-can-also" className={styles.container}>
-      <div className={styles.ellipse} />
-      <Image src={img} alt="Chatter" className={styles.image} />
-      <div className={styles.textContainer}>
-        <h2 className={styles.title}>
-          You can also try chatting with ours. It’s fun!
-        </h2>
-        <Button text="Start Chatting" />
-      </div>
-    </section>
-  );
-};
+const YouCanAlso = () => (
+  <section id="you-can-also" className={styles.container}>
+    <div className={styles.ellipse} />
+    <Image src={img} alt="Chatter" className={styles.image} />
+    <div className={styles.textContainer}>
+      <h2 className={styles.title}>
+        You can also try chatting with ours. It’s fun!
+      </h2>
+      <Button text="Start Chatting" />
+    </div>
+  </section>
+);
 
 export default YouCanAlso;
