de4af65c9b0963391e2aed1254dc29022b6ae219
Michael <michael.d.barto@gmail.com>
2024-12-15 22:10:41 -0500

    finished footer without responsiveness

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <div></div>
-    </footer>
+    <section id="footer" className={styles.container}>
+      <div className={styles.text}>©2024 FAQChat All Rights Reserved.</div>
+      <div className={styles.text2}>
+        <div className={styles.termsOfService}>Terms of Service</div>
+        <div className={styles.divider}></div>
+        <div className={styles.privacyPolicy}>Privacy Policy</div>
+      </div>
+    </section>
   );
 };
 
